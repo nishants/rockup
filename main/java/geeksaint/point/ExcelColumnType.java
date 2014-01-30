4aa0515b952272365b427497f80b4470fb1e78de
nishant <nishant.singh87@gmail.com>
2014-01-30 22:34:14 +0530

    Adding more tests for ExcelColumnType

--- main/java/geeksaint/point/ExcelColumnType.java
+++ main/java/geeksaint/point/ExcelColumnType.java
@@ -3,25 +3,25 @@ package geeksaint.point;
 import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 import org.apache.poi.ss.usermodel.Cell;
 
-import java.math.BigDecimal;
-
 import static java.lang.String.format;
 
 public enum ExcelColumnType {
   STRING {
     @Override
-    public String getCellValue(Cell cell) {
-      if (cell == null) throw new IllegalArgumentException(format("Cell was null at (%d,%d)",
-          cell.getRowIndex(), cell.getColumnIndex()));
+    public Object getCellValue(Cell cell) {
+      if (cell == null) throw new IllegalArgumentException(format("Cell was null"));
 
+      if(cell.getCellType() == Cell.CELL_TYPE_BLANK) return "";
       if (cell.getCellType() == Cell.CELL_TYPE_STRING) return cell.getStringCellValue();
+      if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC && isDate(cell))  return  cell.getDateCellValue().toString();
       if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC) return getNumberAsString(cell);
-      if (cell.getCellType() == Cell.CELL_TYPE_NUMERIC && cellDateFormatted(cell))  return  cell.getDateCellValue().toString();
 
-      throw new UnsupportedOperationException(format("Cell type not supported for %d", cell.getCellType()));
+      throw new UnsupportedOperationException(
+          format("Cannot read string value form cell at(%d,%d) of type %d",cell.getRowIndex(),
+          cell.getColumnIndex(), cell.getCellType()));
     }
 
-    private boolean cellDateFormatted(Cell cell) {
+    private boolean isDate(Cell cell) {
       return HSSFDateUtil.isCellDateFormatted(cell);
     }
 
--- test/java/geeksaint/point/ExcelColumnTypeTest.java
+++ test/java/geeksaint/point/ExcelColumnTypeTest.java
@@ -1,7 +1,12 @@
 package geeksaint.point;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Date;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -9,6 +14,27 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class ExcelColumnTypeTest {
+
+  private final static double A_VALID_CELL_STYLE_DATA_FORMAT = 1 - Double.MIN_VALUE;
+
+  @Rule
+  public ExpectedException thrown = ExpectedException.none();
+
+  @Test
+  public void shouldThrowExceptionIfCellIsNul(){
+    thrown.expect(IllegalArgumentException.class);
+    ExcelColumnType.STRING.getCellValue(null);
+  }
+
+  @Test
+  public void shouldRReturnEmptyStringIfCellTypeIsBlank(){
+    Cell cell = mock(Cell.class);
+
+    when(cell.getCellType()).thenReturn(Cell.CELL_TYPE_BLANK);
+
+    assertThat((String) ExcelColumnType.STRING.getCellValue(cell), is(""));
+  }
+
   @Test
   public void shouldReadStringValuesFromACell(){
     Cell cell = mock(Cell.class);
@@ -19,4 +45,44 @@ public class ExcelColumnTypeTest {
     assertThat((String) ExcelColumnType.STRING.getCellValue(cell), is("cell-value"));
   }
 
+  @Test
+  public void shouldReadDateValuesFromACell(){
+    Cell cell = mock(Cell.class);
+    Date date = new Date();
+
+    dateStyleCell(cell, date);
+
+    assertThat((String) ExcelColumnType.STRING.getCellValue(cell), is(date.toString()));
+  }
+
+  private void dateStyleCell(Cell cell, Date date) {
+    when(cell.getCellType()).thenReturn(Cell.CELL_TYPE_NUMERIC);
+    when(cell.getNumericCellValue()).thenReturn(A_VALID_CELL_STYLE_DATA_FORMAT);
+    when(cell.getDateCellValue()).thenReturn(date);
+
+    CellStyle cellStyle = mock(CellStyle.class);
+    when(cell.getCellStyle()).thenReturn(cellStyle);
+    when(cellStyle.getDataFormat()).thenReturn(CellStyle.ALIGN_GENERAL);
+    when(cellStyle.getDataFormatString()).thenReturn("m/d/y");
+  }
+
+  @Test
+  public void shouldReadValuesFromNumericCell(){
+    Cell cell = mock(Cell.class);
+
+    when(cell.getCellType()).thenReturn(Cell.CELL_TYPE_NUMERIC);
+    when(cell.getNumericCellValue()).thenReturn(9989.9989);
+
+    assertThat((String) ExcelColumnType.STRING.getCellValue(cell), is("9989.9989"));
+  }
+
+  @Test
+  public void shouldThrowExceptionIfCellIsNotNumericStringOrDate(){
+    Cell cell = mock(Cell.class);
+
+    when(cell.getCellType()).thenReturn(Cell.CELL_TYPE_ERROR);
+
+    thrown.expect(UnsupportedOperationException.class);
+    ExcelColumnType.STRING.getCellValue(cell);
+  }
 }
