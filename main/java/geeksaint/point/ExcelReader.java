2cc76ccdca3584a291042f24166cbc7b24773bcf
nishant <nishant.singh87@gmail.com>
2014-01-31 20:57:12 +0530

    Adding logic in excel reader to read a doc

--- main/java/geeksaint/point/ExcelReader.java
+++ main/java/geeksaint/point/ExcelReader.java
@@ -1,37 +1,44 @@
 package geeksaint.point;
 
+import extab.spike.AnnotationProcessor;
+import extab.spike.ItemInstatiator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ExcelReader<T> {
   private Workbook workBook;
-  private Class<T> rowType;
+  private ExcelRowReader<T> rowReader;
 
-  private ExcelReader(String fileName, Class<T> rowType) throws FileNotFoundException, IOException {
-    this.workBook = ExcelFileType.getFileType(fileName)
+  protected ExcelReader(Workbook workBook,
+                        ExcelRowReader<T> rowReader){
+    this.workBook = workBook;
+    this.rowReader = rowReader;
+  }
+
+  protected List<T> readDocument(){
+    List<T> values = new ArrayList<T>();
+    Sheet sheet = workBook.getSheetAt(0);
+    Iterator<Row> rowIterator = sheet.rowIterator();
+    while(rowIterator.hasNext()){
+      values.add(rowReader.getData(rowIterator.next())) ;
+    }
+    return values;
+  }
+
+  public static <T> List<T> read(String fileName, Class<T> rowType) throws IOException {
+    Workbook workBook = ExcelFileType.getFileType(fileName)
         .getWorkbook(new FileInputStream(fileName));
-    this.rowType = rowType;
+    RowDefinition rowDefinition =new RowDefinition(rowType, new AnnotationProcessor());
+    ExcelRowReader <T> rowReader = new ExcelRowReader<T>(rowDefinition, new ItemInstatiator(rowType));
+
+    return (new ExcelReader<T>(workBook, rowReader)).readDocument();
   }
-
-  private List<T> readDocument(){
-    // get sheet
-    // for each row in the sheet
-    // invoke row reader - add the result in the list
-//    Object obj = null;
-//    List<T> result = new ArrayList<T>();
-//    result.add((T)obj);
-
-    return null;
-  }
-
-  public static <T> ExcelReader getExcelReader(String fileName, Class<T> rowType) throws IOException {
-    return new ExcelReader(fileName, rowType);
-  }
-
-
 }
